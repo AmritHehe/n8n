f04dadbdf6b946e1b236c1bcacd8c318ea475523
AmritHehe <hopesalive1506@gmail.com>
2025-09-15 13:32:28 +0530

    added node creation in fe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,42 +2,72 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-
-
-const inititalNodes = [{ 
-  id : 'n1' , 
-  position : { x : 0 , y : 0} , 
-  data : { label : 'Node 1'} , 
-  type : 'input'
-} , 
-{ 
-  id : 'n2' , 
-  position : { x : 100 , y : 100} , 
-  data : { label : 'Node 2'} , 
-
-},
-]
-
-const initialEdges = [{ 
-  id : 'n1-n2' , 
-  source : 'n1' , 
-  target : 'n2' ,
-},]
 import { useState, useCallback } from 'react';
 import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge  , Background , Controls, Position } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import { snapshot } from "node:test";
  
+const inititalNodes = [{ 
+  id : '1' , 
+  position : { x : 0 , y : 0} , 
+  data : { label : 'Node 1'} , 
+  type : 'input'
+} 
+]
+
+const initialEdges = []
+
 
 export default function App() {
-  
+  const [nodes ,setNodes] = useState(inititalNodes)
+  const [edges , setEdges] = useState(initialEdges)
+
+  function addNode(name : string){ 
+    let size = (nodes.length + 1).toString()
+    console.log("size" + size)
+    
+    setNodes(nodes => [...nodes , { 
+      id : size , 
+      position : { x : 200 , y : 200} , 
+      data : { label : name} , 
+      type : 'default'
+    }])
+    setEdges(edges => [...edges , { 
+      id : (Math.random().toFixed(0)).toString(),
+      source : nodes.length.toString(),
+      target : (nodes.length+1).toString()
+    }])
+  }
+  const onNodeChange = useCallback(
+    //@ts-ignore
+  (changes) => setNodes((nodesSnapshot)=>applyNodeChanges(changes , nodesSnapshot))
+   ,[]
+  )
+  const onEdgesChange = useCallback(
+    //@ts-ignore
+  (changes) => setEdges((edgesSnapshot) => applyNodeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  //@ts-ignore
+  const onConnect = useCallback((params)=> setEdges((edgesSnapshot)=> addEdge(params , edgesSnapshot)) ,[], ) 
   return (
-    <div style={{ height: '80vh', width: '80vh' }}>
-      <ReactFlow nodes={inititalNodes}>
-        <Background/>
-        <Controls/>
-      </ReactFlow>
+    <>
+      <div className="flex items-center w-full h-dvh gap-4 bg-amber-50 relative">
+        <div className="flex flex-col absolute z-10 items-center justify-center bg-zinc-800 w-1/6 h-full">
+        <h1 className="font-bold text-xl text-white">Whats Next</h1>
+          
+          <button onClick={()=> {addNode('teligram')}} className="bg-black rounded-lg p-2 m-2 text-white"> telegram </button>
+          <button onClick={()=> {addNode('gmail')}} className="bg-black rounded-lg p-2 m-2 text-white"> gmail </button>
+        </div> 
+        <div className="w-full h-full" >
+          <ReactFlow colorMode="dark" nodes={nodes} edges={edges} onNodesChange={onNodeChange} onEdgesChange={onEdgesChange} onConnect={onConnect} fitView>
+            <Background/>
+            <Controls/>
+          </ReactFlow>
+        </div>
         
-      
-    </div>
+      </div>
+    </>
   );
 }
+
