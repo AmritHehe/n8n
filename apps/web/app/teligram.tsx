7d5ba2904f88e32734f5e558aeb22f8661928b38
AmritHehe <hopesalive1506@gmail.com>
2025-09-16 19:01:55 +0530

    fixed svg typos

--- apps/web/app/teligram.tsx
+++ apps/web/app/teligram.tsx
@@ -10,7 +10,7 @@ export function TeligramNode({ id }: { id: string }){
         <button onClick={deleteNode} className="text-white "><Cross/></button>
 
         <div className="flex items-center justify-center shadow-zinc-600 bg-zinc-300 px-6 py-4 rounded-xl">
-            <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M21 5L2 12.5L9 13.5M21 5L18.5 20L9 13.5M21 5L9 13.5M9 13.5V19L12.2488 15.7229" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+            <svg width="24px" height="24px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M21 5L2 12.5L9 13.5M21 5L18.5 20L9 13.5M21 5L9 13.5M9 13.5V19L12.2488 15.7229" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
             
             <Handle type = "source"  position = {Position.Right}/>
              <Handle type = "target" position = {Position.Left}/>
