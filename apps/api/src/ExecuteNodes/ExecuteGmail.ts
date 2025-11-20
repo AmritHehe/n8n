d7f10207b7955e0b2899171d3438eea2da6e16e4
AmritHehe <hopesalive1506@gmail.com>
2025-11-20 17:26:21 +0530

    fixed localhost url

--- apps/api/src/ExecuteNodes/ExecuteGmail.ts
+++ apps/api/src/ExecuteNodes/ExecuteGmail.ts
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer'
 export async  function gmail(data : any , to : string, subject : string, message : string , awaitMail : boolean, id ?: number , workflowId?: string){ 
     //user ke credentials , 
+      const base = process.env.NEXT_PUBLIC_BACKEND_API;
     try{
         const transporter = nodemailer.createTransport({
             host: data.HOST ,
@@ -16,7 +17,7 @@ export async  function gmail(data : any , to : string, subject : string, message
                 from: 'onboarding@resend.dev',
                 to: to,
                 subject: subject,
-                html: `<strong>${message} please go on the following link to respond to the message https://api-n8n.amrithehe.com/webhook/${id}?workflowId=${workflowId} </strong>`,
+                html: `<strong>${message} please go on the following link to respond to the message ${base}/webhook/${id}?workflowId=${workflowId} </strong>`,
                 
             });
             console.log('Message sent: %s', info.messageId);
