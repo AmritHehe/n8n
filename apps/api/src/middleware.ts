3f848b595353595eff51cacb92d5b527b433a19c
AmritHehe <hopesalive1506@gmail.com>
2025-12-03 15:14:56 +0530

    bug fixes

--- apps/api/src/middleware.ts
+++ apps/api/src/middleware.ts
@@ -2,18 +2,19 @@ import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 export function Usemiddleware( req : Request , res : Response , next : NextFunction){ 
     const token = req.headers.authorization ; 
+    if(!token){ 
+       return res.status(403).json("please sign in first")
+    }
     const JWT_SECRET = process.env.JWT_SECRET! ;
-    console.log(JWT_SECRET)
-    if(token){ 
+    try { 
         const user = jwt.verify(token , JWT_SECRET)
-        if(user){ 
-            //@ts-ignore
-            req.userId = user.id
-        }
-        
+        //@ts-ignore
+        req.userId = user.id
         next()
     }
-    else{ 
-        res.json("user not found")
+    catch(e){ 
+        res.status(403).json({ 
+            error : "invalid token"
+        })
     }
 }
